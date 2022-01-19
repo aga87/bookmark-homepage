f1dbb28858ed786a3607304f7e7cfe406c646829
aga87 <aga.labonarska@outlook.com>
2022-01-19 13:48:28 +0100

    Install Apollo Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Utilize the environment variables defined in the `.env` file
+const {
+  REACT_APP_CONTENTFUL_SPACE_ID: SPACE_ID,
+  REACT_APP_CONTENTFUL_ENVIRONMENT: ENVIRONMENT,
+  REACT_APP_CONTENTFUL_ACCESS_TOKEN: ACCESS_TOKEN
+} = process.env;
+
+const apolloClient = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/${ENVIRONMENT}`,
+  headers: {
+    Authorization: `Bearer ${ACCESS_TOKEN}`
+  },
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,3 +32,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default apolloClient;
