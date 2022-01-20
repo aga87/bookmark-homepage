7a97bdb3f13ee6765d1928e691af43ffa564b162
aga87 <aga.labonarska@outlook.com>
2022-01-20 14:18:09 +0100

    Add functionality for Feature tabs: roving focus with manual activation

--- src/components/Features/FeatureTabs.tsx
+++ src/components/Features/FeatureTabs.tsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetFeatureTabsQuery } from '../../contentful';
+import useRovingFocus from '../../hooks/useRovingFocus';
 import Tab from '../nano/Tab';
+import FeatureTabPanels from './FeatureTabPanels';
 
 const FeatureTabs = (): JSX.Element | null => {
   const { loading, error, data } = useGetFeatureTabsQuery();
+  // Tabs with roving focus and manual activation
+  const [selectedTab, selectTab] = useState(0);
+  const { widgetItemsRefs, handleKeyDown } = useRovingFocus(
+    data?.page?.featureTabsCollection?.items.length || 0,
+    true
+  );
 
-  if (loading || error) return null;
-  if (!data?.page?.featureTabsCollection?.items) return null;
-
-  const { items } = data.page.featureTabsCollection;
-
-  const handleTabClick = () => {
-    console.log('todo');
+  const handleTabClick = (number: number) => {
+    selectTab(number);
   };
 
-  const handleTabKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    console.log('todo', e.target);
-  };
-
-  const tabs = items.map(item => {
+  const tabs = data?.page?.featureTabsCollection?.items.map((item, i) => {
     if (!item?.tabContent || !item?.tabTitle) return null;
     return (
       <li key={item.tabContent.sys.id}>
         <Tab
+          ref={ref => {
+            widgetItemsRefs.current[i] = ref;
+          }}
           title={item.tabTitle}
-          selected={false}
-          handleClick={handleTabClick}
-          handleKeyDown={handleTabKeyDown}
+          selected={i === selectedTab}
+          handleClick={() => handleTabClick(i)}
+          handleKeyDown={handleKeyDown}
           tabPanelId={item.tabContent.sys.id}
         />
       </li>
     );
   });
 
-  return <ul role='tablist'>{tabs}</ul>;
+  if (loading || error || !tabs) return null;
+  return (
+    <div>
+      <ul role='tablist'>{tabs}</ul>
+      <FeatureTabPanels selectedTab={selectedTab} />
+    </div>
+  );
 };
 
 export default FeatureTabs;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MainHero from '../components/MainHero/MainHero';
 import Features from '../components/Features/Features';
 import FeatureTabs from '../components/Features/FeatureTabs';
-import FeatureTabPanels from '../components/Features/FeatureTabPanels';
 import Download from '../components/Download/Download';
 import InstallationCards from '../components/Download/InstallationCards';
 import FAQs from '../components/FAQs/FAQs';
@@ -14,7 +13,6 @@ const Homepage = (): JSX.Element => (
     <MainHero />
     <Features>
       <FeatureTabs />
-      <FeatureTabPanels />
     </Features>
     <Download>
       <InstallationCards />
